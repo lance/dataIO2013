d00f66a7416740a4ccef08f2d9fbbdd397752f42
Lance Ball <lball@redhat.com>
2013-10-30 11:04:29 -0400

    Make it all functional, kind of

--- js/ballot.js
+++ js/ballot.js
@@ -1,93 +1,85 @@
 // Use the vertx event bus to synchronize our models
 var bus = new vertx.EventBus('http://localhost:8080/eventbus');
+bus.mongoAddress = 'demo.mongo';
+
+// Send a message to mongo on the event bus fetching all the records.
+var readCollection = function(bus, collection, options) {
+  var query = {
+    action: 'find',
+    collection: collection.name,
+    matcher: {}
+  }
+  if (options.sort) {
+    query.sort = options.sort;
+  }
+  if (options.matcher) {
+    query.matcher = options.matcher;
+  }
+  bus.send(bus.mongoAddress, query, function(msg) {
+    if (msg['status'] == 'ok') { 
+      var collection = msg['results'];
+      _.each(collection, function(record) {
+        // add the mongo data to our backbone model collection
+        var modelRecord = convertToBackbone(ballot, record);
+
+        // Listen to the event bus for changes to this model
+        console.log(['Registering handler for', modelRecord.url()].join(' '));
+        bus.registerHandler(modelRecord.url(), function(msg) {
+          modelRecord.set(msg);
+        });
+      });
+    } else { alert("There was an error loading the collection: " + msg['message']); }
+  });
+}
+
+// Converts mongo candidate records to backbone models
+var convertToBackbone = function(collection, record) {
+  record.id = record._id;
+  ballot.add(record);
+  return ballot.last();;
+}
+
+// Publish any changed model attributes to the event bus
+var publishModelUpdate = function(bus, model) {
+  // notify listeners of  individual model changes
+  bus.publish(model.url(), model.changedAttributes());
+
+  // a mongodb update message
+  var update = {
+    action: 'update',
+    collection: model.collection.name,
+    criteria: {
+      _id: model.id
+    },
+    objNew: {
+      $set: model.changedAttributes()
+    }
+  }
+  // publish the changes to the event bus, persisting to mongo
+  bus.publish(bus.mongoAddress, update);
+}
 
 /**
- * MongoBackbone can be used to synchronize backbone.js models with
- * a mongodb instance exposed over the vertx event bus. Caution this
- * is not really safe to use in real life. You'll want to have better
- * security over what's allowed over the bus.
- * @constructor
+ * mongoBackbone can be used to synchronize backbone.js models with
+ * a mongodb instance exposed over the vertx event bus. 
+ * Caution this is not safe to use in real life. 
  */
-var MongoBackbone = function(bus, address) {
-
+var mongoBackbone = function(bus) {
   /**
    * Synchronizes the model based on the method provided.
-   * Currently only read and update are supported.
    * @param {string} method The synch method (read or update)
    * @param {{}} model The backbone.js model to synchronize.
    *        This may be a collection or individual model.
    * @param {{}} options Mongodb query options
    */
-  this.sync = function(method, model, options) {
-    
+  return function(method, model, options) {
     // Reads a model or collection from mongo using the event bus..
     if (method === 'read') {
-
-      // Send a message to the mongo address on the vertx event bus
-      // requesting a fetch of all the candidate records.
-      bus.send(address, _constructReadQuery(model, options), function(msg) {
-        if (msg['status'] == 'ok') { 
-
-          // Iterate over the results and add to the collection
-          _.each(msg['results'], function(record) {
-
-            // Set an id property for backbone and
-            // add the record to the collection model
-            record.id = record._id;
-            model.add(record);
-            var modelRecord = model.last();
-
-            // Register a handler on the event bus for the new record. 
-            // When changes are made to the record elsewhere, our handler 
-            // will be called and the model can be updated locally.
-            console.log(['Registering handler for', modelRecord.url()].join(' '));
-            bus.registerHandler(modelRecord.url(), function(msg) {
-              modelRecord.set(msg);
-            });
-
-          });
-        } else { alert("There was an error loading the collection: " + msg['message']); }
-      });
+      readCollection(bus, model, options);
     } 
     else if (method === 'update') {
-      // The model has been changed locally. Publish changes to the 
-      // vertx event bus, informing all clients who have subscribed
-      // to the model's address on the vertx bus this model has changed.
-      bus.publish(model.url(), model.changedAttributes());
-
-      // update mongo via the mongo channel on the vertx event bus
-      var update = {
-        action: 'update',
-        collection: model.collection.name,
-        criteria: {
-          _id: model.id
-        },
-        objNew: {
-          $set: model.changedAttributes()
-        }
-      }
-      // publish the changes to the mongodb address
-      // on the event bus, ensuring that they are persisted
-      bus.publish(address, update);
+      publishModelUpdate(bus, model);
     }
-  }
-
-  /**
-   * @private
-   */
-  var _constructReadQuery = function(model, options) {
-    var query = {
-      action: 'find',
-      collection: model.name,
-      matcher: {}
-    }
-    if (options.sort) {
-      query.sort = options.sort;
-    }
-    if (options.matcher) {
-      query.matcher = options.matcher;
-    }
-    return query;
   }
 
 }
@@ -103,10 +95,9 @@ var Ballot = Backbone.Collection.extend({
 var ballot = new Ballot(); 
 
 bus.onopen = function() {
-  var mongoBackbone = new MongoBackbone(bus, 'demo.mongo');
 
-  // Once the event bus is ready, setup our model synchronization
-  Backbone.sync = mongoBackbone.sync;
+  // Once the event bus is ready, setup model sync
+  Backbone.sync = mongoBackbone(bus)
 
   // As models are added to the collection, display them with the view
   ballot.on('add', function(candidate) {
